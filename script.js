99de10b5b22e03663d6dd585acaab15fee691c02
Samuel Soares Dos Santos <161149946+SamuelDevelop@users.noreply.github.com>
2024-10-11 15:05:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function openNav() {
+
     document.querySelector("main").classList.add("active");
-    document.getElementById("barra-lateral").style.width = "250px";
-    document.querySelector(".openbtn").style.display = "none"; // Esconde o botão ao abrir
+    if (window.innerWidth > 760) {
+        document.getElementById("barra-lateral").style.width = "250px";
+    } else {
+        document.getElementById("barra-lateral").style.width = (window.innerWidth * 0.3) + "px";
+    }
+    document.querySelector(".openbtn").style.display = "none"; 
 }
+
 
 function closeNav() {
     document.querySelector("main").classList.remove("active");
     document.getElementById("barra-lateral").style.width = "0";
-    document.querySelector(".openbtn").style.display = "block"; // Mostra o botão ao fechar
+    document.querySelector(".openbtn").style.display = "block"; 
 }
 
 
